b3a48dc45091a4453f3ae80e3e3408dd744a46e5
Melchizedek Das <melchizedekdas@gmail.com>
2019-03-05 20:29:55 +0530

    get_resource, get_resource_parts implemented

--- src/resource.c
+++ src/resource.c
@@ -6,20 +6,30 @@
  */
 
 #include<stdio.h>
+#include<stdlib.h>
 #include "resource.h"
 #include "constants.h"
+
+#define RESOURCES_PREFIX "../web-inf"
 
 #define META_SUFFIX "_meta"
 #define META_EXTENSION ".txt"
 
 #define RESOURCE_EXTENSION ".html"
 
+#define VERSION_ID_SEPARATOR "_"
 #define DELTA_EXTENSION ".delta"
+
 #define VERSION_ID_SIZE 10
 
 char *get_current_version_id(char *resource_path){
 	char *version_id[VERSION_ID_SIZE];
 	// Open file
+	char *file_path=(char *)malloc(sizeof(char)*(strlen(RESOURCES_PREFIX)+strlen(resource_path)+strlen(META_SUFFIX)+strlen(META_EXTENSION)+1));
+	strcpy(file_path,RESOURCES_PREFIX);
+	strcat(file_path,resource_path);
+	strcat(file_path,META_SUFFIX);
+	strcat(file_path,META_EXTENSION);
 	FILE *fptr = fopen(resource_path, "r");
 	if (fptr == NULL)
 	{
@@ -41,13 +51,45 @@ char *get_current_version_id(char *resource_path){
 }
 
 int get_resource(char *resource_path, char *resource_data){
-	//to be implemented
-
-	return SUCCESS;
+	char *resource_content[BUFFER_SIZE];
+	// Open file
+	char *file_path=(char *)malloc(sizeof(char)*(strlen(RESOURCES_PREFIX)+strlen(resource_path)+strlen(RESOURCE_EXTENSION)+1));
+	strcpy(file_path,RESOURCES_PREFIX);
+	strcat(file_path,resource_path);
+	strcat(file_path,RESOURCE_EXTENSION);
+	FILE *fptr = fopen(resource_path, "r");
+	if (fptr == NULL)
+	{
+		return FAILURE;
+	}
+	int chars_read=0;
+	// Read contents from file
+	while(fgets(resource_content+chars_read, BUFFER_SIZE-chars_read, fptr)){
+		chars_read+=strlen(resource_content+chars_read);
+	}
+	fclose(fptr);
+	return chars_read;
 }
 
 int get_resource_parts(char *resource_path, char *cached_version_id, char *resource_data){
-	//to be implemented
-
-	return SUCCESS;
+	char *resource_content[BUFFER_SIZE];
+	// Open file
+	char *file_path=(char *)malloc(sizeof(char)*(strlen(RESOURCES_PREFIX)+strlen(resource_path)+strlen(RESOURCE_EXTENSION)+1));
+	strcpy(file_path,RESOURCES_PREFIX);
+	strcat(file_path,resource_path);
+	strcat(file_path,VERSION_ID_SEPARATOR);
+	strcat(file_path,cached_version_id);
+	strcat(file_path,DELTA_EXTENSION);
+	FILE *fptr = fopen(resource_path, "r");
+	if (fptr == NULL)
+	{
+		return FAILURE;
+	}
+	int chars_read=0;
+	// Read contents from file
+	while(fgets(resource_content+chars_read, BUFFER_SIZE-chars_read, fptr)){
+		chars_read+=strlen(resource_content+chars_read);
+	}
+	fclose(fptr);
+	return chars_read;
 }
